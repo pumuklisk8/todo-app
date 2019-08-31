b5a21f77445c03edf5a418854e6476fec3f7c0e1
Dani <schmdt.daniel@gmail.com>
2019-08-31 15:43:22 +0200

    toggle display of empty lists, add badge for list headers

--- src/todo_list/List.jsx
+++ src/todo_list/List.jsx
@@ -5,7 +5,7 @@ class List extends Component {
     render() { 
         return (
             <React.Fragment>
-                <h1>{this.props.list.listName}</h1>
+                <h2 className={this.toggleDisplay()}><span className="badge badge-secondary">{this.props.list.listName}</span></h2>
                 <ul className="list-group">
                     {this.props.list.items.map(item =>
                         <ListItem 
@@ -18,6 +18,11 @@ class List extends Component {
                 </ul>
             </React.Fragment>
         );
+    }
+
+    toggleDisplay() {
+        let classes = "m3 ";
+        return this.props.list.items.length > 0 ? classes : classes + "d-none";
     }
 }
  
